5631eaa1c27915bca2ea5865de5be210c75ed182
DavidG <igdrasilenllamas@gmail.com>
2016-02-12 10:49:36 +0100

    Creado metodo para saber el numero de miembros unidos en el mes indicado

--- Club.java
+++ Club.java
@@ -35,4 +35,24 @@ public class Club
     {        
         return members.size();
     }
+    
+    /** 
+     * Calcula el numero de socios que se dieron de alta en un mes determinado. 
+     * El año no nos importa. En caso de que el parametro contenga un valor 
+     * no valido se muestra por pantalla el error.
+     * @param month El mes en el que estamos interesados
+     * @return El numero de socios que se dieron de alta dicho mes
+     */
+    public int joinedMonth(int month){
+        int cont = 0;
+        if(month > 0 && month < 13){
+            for(Membership member: members){
+                if(member.getMonth() == month){cont++;}
+            }
+        }
+        else{
+            System.out.println("El número introducido no pertenece a un mes.");
+        }
+        return cont;
+    }
 }
